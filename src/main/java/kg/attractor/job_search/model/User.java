88f68f3b1bb27bb72b4ae8eee93a82e165787db5
Seitek <seita20078@gmail.com>
2025-03-14 16:37:12 +0600

    Added user model

---
+++ src/main/java/kg/attractor/job_search/model/User.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search.model;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String surname;
+    private Integer age;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String avatar;
+    private String accountType;
+}

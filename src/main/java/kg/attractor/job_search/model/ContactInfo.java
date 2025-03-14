438a1ff55243d91f9ad41c5bfe6bb9bb8c64b571
Seitek <seita20078@gmail.com>
2025-03-14 16:38:52 +0600

    Added ContactInfo model

---
+++ src/main/java/kg/attractor/job_search/model/ContactInfo.java
@@ -0,0 +1,8 @@
+package kg.attractor.job_search.model;
+
+public class ContactInfo {
+    private Integer id;
+    private Integer typeId;
+    private Integer resumeId;
+    private String value;
+}

1367792f80e03df20efe36e0a5adf21bdf2c00ed
Seitek <seita20078@gmail.com>
2025-03-14 16:38:06 +0600

    Added Message model

---
+++ src/main/java/kg/attractor/job_search/model/Message.java
@@ -0,0 +1,10 @@
+package kg.attractor.job_search.model;
+
+import java.time.LocalDateTime;
+
+public class Message {
+    private Integer id;
+    private Integer respondedApplicants;
+    private String content;
+    private LocalDateTime timestamp;
+}

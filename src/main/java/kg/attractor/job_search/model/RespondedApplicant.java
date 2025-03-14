3ea0803d98a8a0d5326104a98dbba64298e7bd75
Seitek <seita20078@gmail.com>
2025-03-14 16:37:52 +0600

    Added RespondedApplicant model

---
+++ src/main/java/kg/attractor/job_search/model/RespondedApplicant.java
@@ -0,0 +1,8 @@
+package kg.attractor.job_search.model;
+
+public class RespondedApplicant {
+    private Integer id;
+    private Integer resumeId;
+    private Integer vacancyId;
+    private Boolean confirmation;
+}

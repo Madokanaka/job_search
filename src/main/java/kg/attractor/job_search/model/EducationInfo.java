99754f13b4212b40382124af139964d0a829a4d7
Seitek <seita20078@gmail.com>
2025-03-14 16:38:17 +0600

    Added EducationInfo model

---
+++ src/main/java/kg/attractor/job_search/model/EducationInfo.java
@@ -0,0 +1,13 @@
+package kg.attractor.job_search.model;
+
+import java.time.LocalDate;
+
+public class EducationInfo {
+    private Integer id;
+    private Integer resumeId;
+    private String institution;
+    private String program;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String degree;
+}

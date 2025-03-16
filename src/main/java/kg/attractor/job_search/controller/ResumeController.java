fd84c820cbcb2437aa5aaa06c2ff5eeec85dc543
Seitek <seita20078@gmail.com>
2025-03-17 00:07:56 +0600

    created ResumeController

---
+++ src/main/java/kg/attractor/job_search/controller/ResumeController.java
@@ -0,0 +1,44 @@
+package kg.attractor.job_search.controller;
+
+import kg.attractor.job_search.dto.ResumeDto;
+import kg.attractor.job_search.model.Resume;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/resumes")
+public class ResumeController {
+
+    @PostMapping
+    public ResponseEntity<?> createResume(@RequestBody ResumeDto resumeDto) {
+        // TODO Логика создания резюме
+        return ResponseEntity.status(HttpStatus.CREATED).body("Resume created");
+    }
+
+    @PutMapping("/{resumeId}")
+    public ResponseEntity<?> editResume(@PathVariable Integer resumeId, @RequestBody ResumeDto resumeDto) {
+        //TODO Логика редактирования резюме
+        return ResponseEntity.ok().body("Resume edited");
+    }
+
+    @DeleteMapping("/{resumeId}")
+    public ResponseEntity<Void> deleteResume(@PathVariable Integer resumeId) {
+        //TODO Логика удаления резюме
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Resume>> getAllResumes() {
+        //TODO Логика получения всех резюме
+        return ResponseEntity.ok(List.of(new Resume()));
+    }
+
+    @GetMapping("/{resumeId}")
+    public ResponseEntity<Resume> getResume(@PathVariable Integer resumeId) {
+        //TODO Логика получения резюме по id
+        return ResponseEntity.ok(new Resume());
+    }
+}

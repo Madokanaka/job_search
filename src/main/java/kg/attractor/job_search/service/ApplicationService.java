20de8fd35ae7278cef96d639a371abfefe4749eb
Seitek <seita20078@gmail.com>
2025-03-17 09:18:03 +0600

    added application service

--- src/main/java/kg/attractor/job_search/service/ApplicationService.java
+++ src/main/java/kg/attractor/job_search/service/ApplicationService.java
@@ -1,4 +1,11 @@
 package kg.attractor.job_search.service;
 
+import kg.attractor.job_search.model.RespondedApplicant;
+
+import java.util.List;
+
 public interface ApplicationService {
+    RespondedApplicant respondToVacancy(Integer vacancyId, Integer resumeId);
+
+    List<RespondedApplicant> getRespondedApplicants(Integer vacancyId);
 }

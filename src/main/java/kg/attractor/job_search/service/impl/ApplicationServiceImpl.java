ae202fb76f7f338d9042ab7b6083842f144ded03
Seitek <seita20078@gmail.com>
2025-03-25 18:01:58 +0600

    fixed logic of application

--- src/main/java/kg/attractor/job_search/service/impl/ApplicationServiceImpl.java
+++ src/main/java/kg/attractor/job_search/service/impl/ApplicationServiceImpl.java
@@ -6,6 +6,7 @@ import kg.attractor.job_search.exception.DatabaseOperationException;
 import kg.attractor.job_search.service.ApplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +15,16 @@ public class ApplicationServiceImpl implements ApplicationService {
     private final ApplicationDao applicationDao;
 
     @Override
+    @Transactional
     public RespondenApplicantDto respondToVacancy(Integer resumeId, Integer vacancyId) {
+        int vacancyCategory = applicationDao.getCategoryByVacancyId(vacancyId);
+        int resumeCategory = applicationDao.getCategoryByResumeId(resumeId);
+        if (vacancyCategory != resumeCategory) {
+            throw new DatabaseOperationException("Vacancy's and resume's category are not the same");
+        }
+        if (applicationDao.findResponseByResumeAndVacancy(resumeId, vacancyId) != null) {
+            throw new DatabaseOperationException("Application already exists");
+        }
         RespondenApplicantDto response = applicationDao.saveResponse(resumeId, vacancyId);
         if (response == null) {
             throw new DatabaseOperationException("Failed to save response or response is null");

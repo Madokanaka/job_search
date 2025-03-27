7979c069ec3760a2d4291e7571834a19cdd16d5d
Seitek <seita20078@gmail.com>
2025-03-27 14:43:37 +0600

    Added annotations to DTOs for validation

--- src/main/java/kg/attractor/job_search/dto/ContactInfoDto.java
+++ src/main/java/kg/attractor/job_search/dto/ContactInfoDto.java
@@ -1,16 +1,23 @@
 package kg.attractor.job_search.dto;
 
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ContactInfoDto {
+    @NotBlank(message = "Contact type ID is required")
+    @Positive(message = "Type ID must be positive")
     private Integer typeId;
+
+    @NotBlank(message = "Contact value is required")
     private String value;
 }
--- src/main/java/kg/attractor/job_search/dto/VacancyDto.java
+++ src/main/java/kg/attractor/job_search/dto/VacancyDto.java
@@ -1,22 +1,38 @@
 package kg.attractor.job_search.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class VacancyDto {
+    @NotBlank(message = "Vacancy name cannot be empty")
     private String name;
+
     private String description;
+
+    @Min(value = 1, message = "Category must be a positive number")
     private Integer categoryId;
+
+    @Positive(message = "Salary must be a positive number")
     private Double salary;
+
+    @NotNull(message = "Experience to is required")
+    @Min(value = 0, message = "Experience from must be at least 0")
     private Integer expFrom;
+
+    @NotNull(message = "Experience to is required")
+    @Min(value = 0, message = "Experience to must be at least 0")
     private Integer expTo;
+
     private Boolean isActive;
 }

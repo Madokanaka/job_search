f948397c7b9a0aa1311fc0aca56099ed6447a461
Seitek <seita20078@gmail.com>
2025-04-01 16:34:49 +0600

    added all the services to resume controller

--- src/main/java/kg/attractor/job_search/controller/ResumeController.java
+++ src/main/java/kg/attractor/job_search/controller/ResumeController.java
@@ -2,7 +2,11 @@ package kg.attractor.job_search.controller;
 
 import jakarta.validation.Valid;
 import kg.attractor.job_search.dto.ResumeDto;
+import kg.attractor.job_search.service.ContactInfoService;
+import kg.attractor.job_search.service.EducationInfoService;
 import kg.attractor.job_search.service.ResumeService;
+import kg.attractor.job_search.service.WorkExperienceInfoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,17 +16,20 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/resumes")
+@RequiredArgsConstructor
 public class ResumeController {
 
     private final ResumeService resumeService;
-
-    public ResumeController(ResumeService resumeService) {
-        this.resumeService = resumeService;
-    }
+    private final ContactInfoService contactInfoService;
+    private final EducationInfoService educationInfoService;
+    private final WorkExperienceInfoService workExperienceInfoService;
 
     @PostMapping("/create")
     public ResponseEntity<?> createResume(@Valid @RequestBody ResumeDto resumeDto, @RequestParam Integer userId) {
-        resumeService.createResume(resumeDto, userId);
+        int resumeId = resumeService.createResume(resumeDto, userId);
+        contactInfoService.createContactInfo(resumeDto.getContactInfoList(), resumeId);
+        educationInfoService.createEducationInfo(resumeDto.getEducationInfoList(), resumeId);
+        workExperienceInfoService.createWorkExperienceInfo(resumeDto.getWorkExperienceInfoList(), resumeId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body("Resume created");
 
@@ -31,11 +38,23 @@ public class ResumeController {
     @PutMapping("/{resumeId}/edit")
     public ResponseEntity<?> editResume(@PathVariable Integer resumeId, @Valid @RequestBody ResumeDto resumeDto) {
         resumeService.editResume(resumeId, resumeDto);
+        contactInfoService.deleteContactInfoByResumeId(resumeId);
+        educationInfoService.deleteEducationInfoByResumeId(resumeId);
+        workExperienceInfoService.deleteWorkExperienceInfoByResumeId(resumeId);
+
+        contactInfoService.createContactInfo(resumeDto.getContactInfoList(), resumeId);
+        educationInfoService.createEducationInfo(resumeDto.getEducationInfoList(), resumeId);
+        workExperienceInfoService.createWorkExperienceInfo(resumeDto.getWorkExperienceInfoList(), resumeId);
+
         return ResponseEntity.status(HttpStatus.OK).body("Resume was edited");
     }
 
     @DeleteMapping("/{resumeId}/delete")
     public ResponseEntity<?> deleteResume(@PathVariable Integer resumeId) {
+        contactInfoService.deleteContactInfoByResumeId(resumeId);
+        educationInfoService.deleteEducationInfoByResumeId(resumeId);
+        workExperienceInfoService.deleteWorkExperienceInfoByResumeId(resumeId);
+
         resumeService.deleteResume(resumeId);
 
         return ResponseEntity.status(HttpStatus.OK).body("Resume was deleted");

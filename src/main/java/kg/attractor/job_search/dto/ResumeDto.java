9a737cd1accb71c0a682812a5111fc4cf4726953
Seitek <seita20078@gmail.com>
2025-04-15 13:15:06 +0600

    changed ResumeDto contact info from list to own values

--- src/main/java/kg/attractor/job_search/dto/ResumeDto.java
+++ src/main/java/kg/attractor/job_search/dto/ResumeDto.java
@@ -1,9 +1,11 @@
 package kg.attractor.job_search.dto;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,15 +20,15 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResumeDto {
-    @NotBlank(message = "Resume name is required")
+    @NotBlank(message = "Имя резюме необходимо")
     private String name;
 
-    @Min(value = 1, message = "Category must be a positive number")
-    @NotNull(message = "Category ID is required")
+    @Min(value = 1, message = "Категория должна быть положительной")
+    @NotNull(message = "ID категории необходим")
     private Integer categoryId;
 
-    @Positive(message = "Salary must be a positive number")
-    @NotNull(message = "Salary amount is required")
+    @Positive(message = "Зарплата должна быть положительной")
+    @NotNull(message = "Поле зарплаты необходимо")
     private Double salary;
 
     private Boolean isActive;
@@ -37,6 +39,15 @@ public class ResumeDto {
     @Valid
     private List<WorkExperienceInfoDto> workExperienceInfoList;
 
-    @Valid
-    private List<ContactInfoDto> contactInfoList;
+    @Email(message = "email должен быть правильного формата")
+    private String contactEmail;
+
+    @Pattern(regexp = "\\+?[0-9]{10,20}", message = "Invalid phone number format")
+    private String phoneNumber;
+
+    private String linkedIn;
+
+    private String telegram;
+
+    private String facebook;
 }

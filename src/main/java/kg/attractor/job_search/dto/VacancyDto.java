707256ef1271d149e63390bf9a5caf4cf39f103d
Seitek <seita20078@gmail.com>
2025-04-18 02:57:20 +0600

    added new field to vacancy dto

--- src/main/java/kg/attractor/job_search/dto/VacancyDto.java
+++ src/main/java/kg/attractor/job_search/dto/VacancyDto.java
@@ -9,6 +9,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 
 @Data
 @Builder
@@ -36,6 +38,11 @@ public class VacancyDto {
     @NotNull(message = "Experience to is required")
     @Min(value = 0, message = "Experience to must be at least 0")
     private Integer expTo;
+
+    public Integer authorId;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime updateTime;
 
     private Boolean isActive;
 }

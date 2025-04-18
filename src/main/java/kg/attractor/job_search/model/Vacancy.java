8ed239a9cfa8029bec5244097b863a2b14ff2f32
Seitek <seita20078@gmail.com>
2025-04-18 18:19:15 +0600

    updated application yaml file

--- src/main/java/kg/attractor/job_search/model/Vacancy.java
+++ src/main/java/kg/attractor/job_search/model/Vacancy.java
@@ -1,5 +1,6 @@
 package kg.attractor.job_search.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;

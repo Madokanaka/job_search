b98a4cf479bb8c7c55441932202158131354de10
Seitek <seita20078@gmail.com>
2025-04-25 16:20:41 +0600

    added category service and dto

---
+++ src/main/java/kg/attractor/job_search/service/CategoryService.java
@@ -0,0 +1,16 @@
+package kg.attractor.job_search.service;
+
+import kg.attractor.job_search.dto.CategoryDto;
+import kg.attractor.job_search.model.Category;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public interface CategoryService {
+    List<CategoryDto> findAll();
+    Optional<Category> findById(Integer id);
+    Map<Integer, String> getCategories();
+
+    boolean existsByCategoryId(Integer categoryId);
+}

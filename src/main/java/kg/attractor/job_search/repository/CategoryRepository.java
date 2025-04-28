701d68e368034b86b9e932ca62224c6e4d139a96
Seitek <seita20078@gmail.com>
2025-04-29 00:39:22 +0600

    added new method to get the name of category

--- src/main/java/kg/attractor/job_search/repository/CategoryRepository.java
+++ src/main/java/kg/attractor/job_search/repository/CategoryRepository.java
@@ -2,11 +2,14 @@ package kg.attractor.job_search.repository;
 
 import kg.attractor.job_search.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
+    @Query("SELECT c.name FROM Category c WHERE c.id = :id")
+    Optional<String> findNameById(@Param("id") Integer id);
 }

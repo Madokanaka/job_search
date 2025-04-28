c21749b303c2a0fbcd840e15a6155272d48d8ed6
Seitek <seita20078@gmail.com>
2025-04-29 01:56:15 +0600

    added new Exceptions to handle various situations

---
+++ src/main/java/kg/attractor/job_search/exception/IllegalEducationDatesException.java
@@ -0,0 +1,7 @@
+package kg.attractor.job_search.exception;
+
+public class IllegalEducationDatesException extends RuntimeException {
+    public IllegalEducationDatesException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/kg/attractor/job_search/exception/IllegalYearsException.java
@@ -0,0 +1,7 @@
+package kg.attractor.job_search.exception;
+
+public class IllegalYearsException extends RuntimeException {
+    public IllegalYearsException(String message) {
+        super(message);
+    }
+}

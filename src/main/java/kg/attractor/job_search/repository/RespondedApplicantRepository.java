6a6ab31ea7b85e44ee5540e46e958a874295b28f
Seitek <seita20078@gmail.com>
2025-04-29 11:25:59 +0600

    added method to repository to check

--- src/main/java/kg/attractor/job_search/repository/RespondedApplicantRepository.java
+++ src/main/java/kg/attractor/job_search/repository/RespondedApplicantRepository.java
@@ -4,6 +4,10 @@ import kg.attractor.job_search.model.RespondedApplicant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RespondedApplicantRepository extends JpaRepository<RespondedApplicant, Integer> {
+    Optional<RespondedApplicant> findByResumeIdAndVacancyId(Integer resumeId, Integer vacancyId);
+
 }

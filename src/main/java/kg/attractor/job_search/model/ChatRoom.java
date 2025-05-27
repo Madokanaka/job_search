7935d6cd4d2d98cad62631c94a25a5c689f2a1fb
Seitek <seita20078@gmail.com>
2025-05-27 11:53:40 +0600

    added chatroom model

---
+++ src/main/java/kg/attractor/job_search/model/ChatRoom.java
@@ -0,0 +1,31 @@
+package kg.attractor.job_search.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "chat_rooms")
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user1_id", nullable = false)
+    private User user1;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user2_id", nullable = false)
+    private User user2;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}

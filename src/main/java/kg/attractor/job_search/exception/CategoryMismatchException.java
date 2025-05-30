3e09230233d578e50f0a029a891acab8f993af84
Seitek <seita20078@gmail.com>
2025-05-30 15:39:35 +0600

    implemented application logic

---
+++ src/main/java/kg/attractor/job_search/exception/CategoryMismatchException.java
@@ -0,0 +1,7 @@
+package kg.attractor.job_search.exception;
+
+public class CategoryMismatchException extends RuntimeException {
+    public CategoryMismatchException(String message) {
+        super(message);
+    }
+}
